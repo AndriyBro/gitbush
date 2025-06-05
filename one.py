bc4e3c35153070b3f13efefcb8762ddd89d31ed3
Andrii <brovinandriy@gmail.com>
2025-06-05 17:58:39 +0300

    в

--- one.py
+++ one.py
@@ -1,2 +1,48 @@
-print("Hello")
-print('sdfsdf')
+import sqlite3
+
+conn = sqlite3.connect("university.db")
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS students(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               name TEXT,
+               age INTEGER,
+               major TEXT
+               )
+''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS courses(
+               course_id INTEGER PRIMARY KEY AUTOINCREMENT,
+               course_name TEXT,
+               instructor TEXT
+               )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS student_courses (
+               student_id INTEGER,
+               course_id INTEGER,
+               FOREGIN KEY (student_id) REFERENCES students(id),
+               FOREGIN KEY (course_id) REFERENCES courses(course_id),
+               PRIMARY KEY (student_id, course_id)
+               
+               )
+
+''')
+
+while True:
+    print("1. Додати нового студента")
+    print("2. Додати новий курс")
+    print("3. Показати список студентів")
+    print("4. Показати курси")
+    print("5. Зареєструвати на курс")
+    print("6. Показати студентів на курсі")
+    print("7. Вихід")
+
+    choice = input("Оберіть від 1 до 7")
+
+    if choice =="1":
+        name= input("Введіть імя")
+        age= int(input("Введіть вік"))
+    
